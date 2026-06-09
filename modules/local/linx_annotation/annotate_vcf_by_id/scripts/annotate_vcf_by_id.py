ab83fe340ecb93c8f5c149496548807a537d44fc
kristinebilgrav <kristinebilgrav1@gmail.com>
2026-06-09 16:46:53 +0200

    review

--- modules/local/linx_annotation/annotate_vcf_by_id/scripts/annotate_vcf_by_id.py
+++ modules/local/linx_annotation/annotate_vcf_by_id/scripts/annotate_vcf_by_id.py
@@ -3,7 +3,7 @@ import pysam
 import csv
 
 """
-This script aims to annotate a vcf using:
+This script aims to annotate the infor field of a vcf based on the ID field, using:
 1. a VCF file following the VCF specifications, where the ID field will be used to annoate entries
 2. a TSV file where the ids to annotate is always the first column and the remaining are new INFO fields: ex.: ID, TAG1, TAG2, TAG3
 3. a TXT file with the header lines of the tags added, following the VCF specifications (i.e. ##INFO=<ID=ID,Number=number,Type=type,Description="description")
@@ -14,32 +14,72 @@ This means that the ‘Flag’ type indicates that the INFO field does not conta
 """
 
 
-# parse tsv file
-def parse_tsv(tsv_file: str) -> dict:
+def convert_types(row: dict) -> dict:
+    # convert numeric strings to ints
+    for key, value in row.items():
+        try:
+            row[key] = int(value)
+        except ValueError:
+            try:
+                row[key] = float(value)
+            except ValueError:
+                row[key] = str(value)
+    # add float
+    return row
 
-    """Parses the tsv file into a dictionary with the id as key and the remaining columns as values in a nested dict"""
+
+def update_vcf_header(
+    vcf_in: pysam.VariantFile, header_file: click.Path
+) -> pysam.VariantHeader:
+    # add new header lines to original header from header file
+    with open(header_file, "r") as hf:
+        new_header_lines = hf.read().splitlines()
+
+    for line in new_header_lines:
+        vcf_in.header.add_line(line)
+    merged_header = vcf_in.header
+
+    return merged_header
+
+
+def update_info_field(
+    record: pysam.VariantRecord, tsv_dict: dict
+) -> pysam.VariantRecord:
+    if record.id in tsv_dict:
+        annotations = tsv_dict[record.id]
+        record.info.update(annotations)
+    return record
+
+
+# parse tsv file
+def parse_tsv(tsv_file_path: click.Path) -> dict:
+    """Parses the tsv file into a dictionary (id as key and the remaining columns as values in a nested dict) compatible with annotation with pysam"""
 
     tsv_dict = {}
-    with open(tsv_file, newline='') as f:
-        reader = csv.DictReader(f, delimiter="\t") # convert each tsv row to dict
+    with open(tsv_file_path, newline="") as f:
+        # convert each tsv row to dict
+        reader = csv.DictReader(f, delimiter="\t")
 
         id_column = reader.fieldnames[0]
         for row in reader:
-            key = row.pop(id_column) # removes id column from dict, returns id (value)
+            # removes id column from dict, returns id (value)
+            key = row.pop(id_column)
 
             # convert numeric strings to ints
-            values = { k: int(v) if v.isdigit() else v
-                      for k, v in row.items() }
+            values = convert_types(row)
 
             # avoid overwriting if id has multiple annotations
             if key not in tsv_dict:
                 tsv_dict[key] = values
 
-            else: # append new value to existing annotations
+            else:
+                # append new value to existing annotations
                 for field, value in values.items():
                     if tsv_dict[key][field] == value:
                         continue
-                    current_entry = tsv_dict[key][field] # current entry for column
+
+                    # current entry for column
+                    current_entry = tsv_dict[key][field]
                     if isinstance(current_entry, list):
                         current_entry.append(value)
                     else:
@@ -53,50 +93,83 @@ def parse_tsv(tsv_file: str) -> dict:
 
     return tsv_dict
 
-def annotate_vcf(vcf_file: click.Path, header_file: click.Path, tsv_dict: dict, output_file: click.Path) -> tuple:
 
-    """ Annotate VCF file using the tsv_dict and header_file, and save the annotated VCF to output_file. Returns the path to the annotated VCF and its index file"""
+def annotate_vcf(
+    vcf_file: click.Path,
+    header_file: click.Path,
+    tsv_dict: dict,
+    output_file: click.Path,
+) -> tuple:
+    """Annotate VCF file using the tsv_dict and header_file, and save the annotated VCF to output_file. Returns the path to the annotated VCF and its index file"""
 
-    vcf_in: pysam.VariantFile = pysam.VariantFile(vcf_file, 'rb')
+    vcf_in: pysam.VariantFile = pysam.VariantFile(vcf_file, "rb")
 
-    # add new header lines to original header from header file
-    with open(header_file, 'r') as hf:
-        new_header_lines = hf.read().splitlines()
+    merged_header = update_vcf_header(vcf_in, header_file)
 
-    for line in new_header_lines:
-        vcf_in.header.add_line(line)
-    merged_header = vcf_in.header
-
-    vcf_out: pysam.VariantFile = pysam.VariantFile( output_file, "w", header=merged_header ) # auto-detects compression from file extension
+    # pysam auto-detects compression from file extension
+    vcf_out: pysam.VariantFile = pysam.VariantFile(
+        output_file, "w", header=merged_header
+    )
 
     for record in vcf_in:
-        if record.id in list(tsv_dict.keys()):
-            annotations = tsv_dict[record.id]
-            record.info.update(annotations)
+        record = update_info_field(record, tsv_dict)
         vcf_out.write(record)
 
     vcf_in.close()
     vcf_out.close()
+
+    # write index file for annotated vcf
     output_index = pysam.tabix_index(output_file, preset="vcf", force=True)
 
     return output_file, output_index
 
+
 @click.command()
 
 # get input files
-@click.option("-v", "--vcf_file", type=click.Path(exists=True), help="The VCF file to annotate", required=True)
-@click.option("-t", "--tsv_file", type=click.Path(exists=True), help="The TSV file with annotations", required=True)
-@click.option("-h", "--header_file", type=click.Path(exists=True), help="A txt file with new header lines for the annotations", required=True)
-@click.option("-o", "--output_file", type=click.Path(), help="name of output annotated VCF file", required=True)
+@click.option(
+    "-v",
+    "--vcf_file",
+    type=click.Path(exists=True),
+    help="The VCF file to annotate",
+    required=True,
+)
+@click.option(
+    "-t",
+    "--tsv_file",
+    type=click.Path(exists=True),
+    help="The TSV file with annotations",
+    required=True,
+)
+@click.option(
+    "-h",
+    "--header_file",
+    type=click.Path(exists=True),
+    help="A txt file with new header lines for the annotations",
+    required=True,
+)
+@click.option(
+    "-o",
+    "--output_file",
+    type=click.Path(),
+    help="name of output annotated VCF file",
+    required=True,
+)
 
 # run
-def main(vcf_file: click.Path, header_file: click.Path, tsv_file: click.Path, output_file: click.Path) -> None:
+def main(
+    vcf_file: click.Path,
+    header_file: click.Path,
+    tsv_file: click.Path,
+    output_file: click.Path,
+) -> None:
 
     # parse tsv file
     tsv_dict = parse_tsv(tsv_file)
 
     # annotate vcf
     annotate_vcf(vcf_file, header_file, tsv_dict, output_file)
+
 
 # main
 if __name__ == "__main__":
