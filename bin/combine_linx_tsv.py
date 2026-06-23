f580d8b48c4c17c5fbfa6ad6ac1da5e5cb00be0f
kristinebilgrav <kristinebilgrav1@gmail.com>
2026-06-23 13:17:48 +0200

    remove unneeded functions

--- bin/combine_linx_tsv.py
+++ bin/combine_linx_tsv.py
@@ -23,34 +23,6 @@ The output is a merged tsv file with the following columns:
 """
 
 
-def rename_columns(df: pd.DataFrame, rename_dict: dict[str, str]) -> pd.DataFrame:
-    """Renames columns in a dataframe based on a provided mapping dictionary"""
-
-    df.rename(columns=rename_dict, inplace=True)
-
-    return df
-
-
-def replace_values(
-    df: pd.DataFrame, column: str, replace_dict: dict[str, int]
-) -> pd.DataFrame:
-    """Replaces values in a specified column of a dataframe based on a provided mapping dictionary"""
-
-    df[column] = df[column].replace(replace_dict)
-
-    return df
-
-
-def merge_dataframes(
-    df1: pd.DataFrame, df2: pd.DataFrame, left_on: str, right_on: str, how: str
-) -> pd.DataFrame:
-    """Merges two dataframes based on specified columns and merge method"""
-
-    merged_df = df1.merge(df2, left_on=left_on, right_on=right_on, how=how)
-
-    return merged_df
-
-
 def merge_linx_files(
     fusion_file: click.Path, breakend_file: click.Path, sv_file: click.Path
 ) -> pd.DataFrame:
@@ -62,24 +34,14 @@ def merge_linx_files(
     svs: pd.DataFrame = pd.read_csv(sv_file, sep="\t", dtype=str)
 
     # rename columns to desired header names in subsequenct VCF annotation step
-    fusions = rename_columns(fusions, {"name": "FUSION_NAME", "reported": "REPORTED"})
+    fusions.rename(columns={"name": "FUSION_NAME", "reported": "REPORTED"}, inplace=True)
 
     # change reported column to 0/1 to adhere to pysam specifications
-
-    fusions = replace_values(fusions, "REPORTED", {"false": 0, "true": 1})
+    fusions["REPORTED"] = fusions["REPORTED"].replace({"false": 0, "true": 1})
 
     # merge fusions and breakends on 'fivePrimeBreakendId' & 'threePrimeBreakendId' and 'id' columns
-    fusion_fivebreakend: pd.DataFrame = merge_dataframes(
-        fusions, breakends, left_on="fivePrimeBreakendId", right_on="id", how="left"
-    )
-
-    fusion_fivebreakend: pd.DataFrame = merge_dataframes(
-        fusions, breakends, left_on="fivePrimeBreakendId", right_on="id", how="left"
-    )
-
-    fusion_threebreakend: pd.DataFrame = merge_dataframes(
-        fusions, breakends, left_on="threePrimeBreakendId", right_on="id", how="left"
-    )
+    fusion_fivebreakend: pd.DataFrame = fusions.merge(breakends, left_on="fivePrimeBreakendId", right_on="id", how="left")
+    fusion_threebreakend: pd.DataFrame = fusions.merge(breakends, left_on="threePrimeBreakendId", right_on="id", how="left")
 
     # concatenate fusion_fivebreakend and fusion_threebreakend to get all fusions
     fusion_breakend_merge: pd.DataFrame = pd.concat(
@@ -87,9 +49,7 @@ def merge_linx_files(
     )
 
     # merge fusion_breakend_merge with svs on 'svId' column in sv file
-    fusion_breakend_svs_merge: pd.DataFrame = merge_dataframes(
-        fusion_breakend_merge, svs, left_on="svId", right_on="svId", how="left"
-    )
+    fusion_breakend_svs_merge: pd.DataFrame = fusion_breakend_merge.merge(svs, left_on="svId", right_on="svId", how="left")
 
     # keep only the relevant columns for the final output - for debug: add columns svId, fivePrimeBreakendId, threePrimeBreakendId
     result: pd.DataFrame = fusion_breakend_svs_merge[
